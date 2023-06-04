61d5e646fff15f0676bf7089313edfd71f6e259f
coolpythoncodes <coolpythoncodes@gmail.com>
2023-06-04 18:43:15 +0100

    wagmi and web3modal config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { WagmiConfig } from "wagmi";
 import Layout from "~/modules/common/components/layout";
 import "~/styles/globals.css";
+import { wagmiConfig } from "~/utils/config";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <div className="pt-[55px] md:pt-20">
-        <Component {...pageProps} />
-      </div>
-    </Layout>
+    <WagmiConfig config={wagmiConfig}>
+      <Layout>
+        <div className="pt-[55px] md:pt-20">
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </WagmiConfig>
   );
 };
 
