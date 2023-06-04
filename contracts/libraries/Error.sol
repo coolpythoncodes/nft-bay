78ce3fbd7bff4c6c17633501ed92ce7576dbc459
coolpythoncodes <coolpythoncodes@gmail.com>
2023-06-05 00:52:15 +0100

    nft marketplace smart contract to buy, sell, bid and auction nft

---
+++ contracts/libraries/Error.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+error ErrPriceZero();
+
+error ErrListingFee();
+
+error ErrAuctionStartTimeWrong();
+
+error ErrAuctionEndTimeWrong();
+
+error ErrPriceNotEqual();
+
+error ErrItemNotListed();
+
+error ErrItemIsAuction();
+
+error ErrAuctionEnded();
+
+error ErrInvalidBid();
+
+error ErrHasNotAuctionEnded();
+
+error ErrInvalidCaller();
+
+error ErrItemListed();
---
+++ contracts/libraries/Events.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+library Events {
+     event MarketItemCreated(
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        address payable creator,
+        uint256 price
+    );
+
+    event MarketItemSold(
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        address payable seller,
+        address payable buyer,
+        uint256 price
+    );
+
+    event PlacedBid (
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        address  payable indexed bidder,
+        uint256 price
+    );
+
+    event AuctionEnded (
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        address  payable indexed nftOwner,
+        address  payable  highestBidder,
+        uint256 price
+    );
+}
